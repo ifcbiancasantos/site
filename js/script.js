6431e78bf96d9ae1b36d4a1585de8ee07fa07d2b
ReehLimas <reehlimasil08@gmail.com>
2023-08-09 16:45:36 -0300

    site

--- js/script.js
+++ js/script.js
@@ -8,3 +8,23 @@ function menuShow(){
         document.querySelector('.icon').src = "imgs/close_white_36dp.svg";
     }
 }
+
+
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 3){
+        cont = 1 
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
